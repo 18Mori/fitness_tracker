ed85868786633ece69b64d1bed44063af2b1f45e
jamesMuriithi <jamesmuriithi777@gmail.com>
2025-08-20 10:17:28 +0300

    feat: added login,logout and register - done with setup

--- velo_app/views.py
+++ velo_app/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .form import UserRegistrationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import authenticate
+from django.views.decorators.http import require_http_methods
+from django.contrib import messages
+from django.views.decorators.http import require_POST
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def home(request):
+  return render(request, 'home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+          user = form.save()
+          auth_login(request, user)
+          return redirect('home')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'register.html', {'form': form})
+  
+@require_http_methods(["GET", "POST"])
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+      
+    if request.method == 'POST':
+      username = request.POST.get('username')
+      password = request.POST.get('password')
+      remember_me = request.POST.get('remember_me', False)
+      user = authenticate(request, username=username, password=password)
+
+      if user is not None:
+          auth_login(request, user)
+          if remember_me:
+              request.session.set_expiry(1209600)  # 2 weeks
+          return redirect('home')
+      else:
+          messages.error(request, 'Invalid username or password.')
+    return render(request, 'auth/login.html')
+
+@require_POST
+@login_required
+def logout_user(request):
+  logout(request)
+  return redirect('home')
--- velo_pj/settings.py
+++ velo_pj/settings.py
@@ -127,3 +127,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# ExerciseDB API Key
+EXERCISEDB_API_KEY = os.getenv('EXERCISEDB_API_KEY')
+EXERCISEDB_API_HOST = os.getenv('EXERCISEDB_API_HOST')
